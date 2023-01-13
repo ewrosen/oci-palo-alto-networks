bf7dae7c228817c764d8ddb131a7ceb04866177d
Eric <ewrosen@gmail.com>
2023-01-13 12:41:27 -0500

    Removing and DB vm's

--- single-arm-use-case/compute.tf
+++ single-arm-use-case/compute.tf
@@ -55,83 +55,83 @@ resource "oci_core_vnic_attachment" "untrust_vnic_attachment" {
 }
 
 # ------ Create Web Standalone VMs
-resource "oci_core_instance" "web-vms" {
-  count = 1
-
-  availability_domain = ( var.availability_domain_name != "" ? var.availability_domain_name : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
-  compartment_id      = var.compute_compartment_ocid
-  display_name        = "${var.vm_display_name_web}-${count.index + 1}"
-  shape               = var.spoke_vm_compute_shape
-  fault_domain        = data.oci_identity_fault_domains.fds.fault_domains[count.index].name
-
-  dynamic "shape_config" {
-    for_each = local.is_spoke_flex_shape
-    content {
-      ocpus = shape_config.value
-    }
-  }
-
-  create_vnic_details {
-    subnet_id              = local.use_existing_network ? var.web_transit_subnet_id : oci_core_subnet.web_lb-subnet[0].id
-    display_name           = var.vm_display_name_web
-    assign_public_ip       = true
-    nsg_ids                = [oci_core_network_security_group.nsg_web.id]
-    skip_source_dest_check = "true"
-  }
-
-  source_details {
-    source_type             = "image"
-    source_id               = data.oci_core_images.InstanceImageOCID.images[1].id
-    boot_volume_size_in_gbs = "50"
-  }
-
-  launch_options {
-    network_type = var.instance_launch_options_network_type
-  }
-
-  metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-  }
-
-}
-
+# resource "oci_core_instance" "web-vms" {
+#  count = 1
+#
+#  availability_domain = ( var.availability_domain_name != "" ? var.availability_domain_name : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+#  compartment_id      = var.compute_compartment_ocid
+#  display_name        = "${var.vm_display_name_web}-${count.index + 1}"
+#  shape               = var.spoke_vm_compute_shape
+#  fault_domain        = data.oci_identity_fault_domains.fds.fault_domains[count.index].name
+#
+#  dynamic "shape_config" {
+#    for_each = local.is_spoke_flex_shape
+#    content {
+#      ocpus = shape_config.value
+#    }
+#  }
+#
+#  create_vnic_details {
+#    subnet_id              = local.use_existing_network ? var.web_transit_subnet_id : oci_core_subnet.web_lb-subnet[0].id
+#    display_name           = var.vm_display_name_web
+#    assign_public_ip       = true
+#    nsg_ids                = [oci_core_network_security_group.nsg_web.id]
+#    skip_source_dest_check = "true"
+#  }
+#
+#  source_details {
+#    source_type             = "image"
+#    source_id               = data.oci_core_images.InstanceImageOCID.images[1].id
+#    boot_volume_size_in_gbs = "50"
+#  }
+#
+#  launch_options {
+#    network_type = var.instance_launch_options_network_type
+#  }
+#
+#  metadata = {
+#    ssh_authorized_keys = var.ssh_public_key
+#  }
+#
+#}
+#
 # ------ Create DB Standalone VMs
-resource "oci_core_instance" "db-vms" {
-  count = 1
-
-  availability_domain = ( var.availability_domain_name != "" ? var.availability_domain_name : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
-  compartment_id      = var.compute_compartment_ocid
-  display_name        = "${var.vm_display_name_db}-${count.index + 1}"
-  shape               = var.spoke_vm_compute_shape
-  fault_domain        = data.oci_identity_fault_domains.fds.fault_domains[count.index].name
-
-  dynamic "shape_config" {
-    for_each = local.is_spoke_flex_shape
-    content {
-      ocpus = shape_config.value
-    }
-  }
-
-  create_vnic_details {
-    subnet_id              = local.use_existing_network ? var.db_transit_subnet_id : oci_core_subnet.db_private-subnet[0].id
-    display_name           = var.vm_display_name_db
-    assign_public_ip       = false
-    nsg_ids                = [oci_core_network_security_group.nsg_db.id]
-    skip_source_dest_check = "true"
-  }
-
-  source_details {
-    source_type             = "image"
-    source_id               = data.oci_core_images.InstanceImageOCID.images[1].id
-    boot_volume_size_in_gbs = "50"
-  }
-
-  launch_options {
-    network_type = var.instance_launch_options_network_type
-  }
-
-  metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-  }
-
-}
+#resource "oci_core_instance" "db-vms" {
+#  count = 1
+#
+# availability_domain = ( var.availability_domain_name != "" ? var.availability_domain_name : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+#  compartment_id      = var.compute_compartment_ocid
+#  display_name        = "${var.vm_display_name_db}-${count.index + 1}"
+#  shape               = var.spoke_vm_compute_shape
+#  fault_domain        = data.oci_identity_fault_domains.fds.fault_domains[count.index].name
+#
+#  dynamic "shape_config" {
+#    for_each = local.is_spoke_flex_shape
+#    content {
+#      ocpus = shape_config.value
+#    }
+#  }
+#
+#  create_vnic_details {
+#    subnet_id              = local.use_existing_network ? var.db_transit_subnet_id : oci_core_subnet.db_private-subnet[0].id
+#    display_name           = var.vm_display_name_db
+#    assign_public_ip       = false
+#    nsg_ids                = [oci_core_network_security_group.nsg_db.id]
+#    skip_source_dest_check = "true"
+#  }
+#
+#  source_details {
+#    source_type             = "image"
+#    source_id               = data.oci_core_images.InstanceImageOCID.images[1].id
+#    boot_volume_size_in_gbs = "50"
+#  }
+#
+#  launch_options {
+#    network_type = var.instance_launch_options_network_type
+#  }
+#
+#  metadata = {
+#    ssh_authorized_keys = var.ssh_public_key
+#  }
+#
+#}
